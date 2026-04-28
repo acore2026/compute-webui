f3c06ab41790cfa15f9b12d371afefcdef3321f5
aicore <aicore@2026.com>
2026-04-28 20:56:43 +0800

    ip fix

--- composables/passiveVideoRtcConfig.ts
+++ composables/passiveVideoRtcConfig.ts
@@ -4,11 +4,19 @@ export const DEFAULT_PASSIVE_VIDEO_ICE_SERVER_URLS = [
 ]
 
 export const LOCALHOST_PASSIVE_VIDEO_ICE_SERVERS: RTCIceServer[] = [
-  { urls: 'stun:127.0.0.1:3478' },
+  // { urls: 'stun:127.0.0.1:3478' },
+  // {
+  //   urls: 'turn:127.0.0.1:3478?transport=udp',
+  //   username: 'sandboxdemo',
+  //   credential: 'sandboxdemo'
+  // }
   {
-    urls: 'turn:127.0.0.1:3478?transport=udp',
-    username: 'sandboxdemo',
-    credential: 'sandboxdemo'
+    "urls": "stun:101.245.78.174:28002"
+  },
+  {
+    "urls": "turn:101.245.78.174:28002?transport=udp",
+    "username": "cloudproxy",
+    "credential": "f41bd6b00f9fe5b5980197d793699aea"
   }
 ]
 
@@ -25,10 +33,16 @@ export function buildPassiveVideoRtcConfiguration(hostname = resolveRuntimeHostn
 
   const iceServers: RTCIceServer[] = useLocalTurn
     ? [
-        ...LOCALHOST_PASSIVE_VIDEO_ICE_SERVERS,
-        ...DEFAULT_PASSIVE_VIDEO_ICE_SERVER_URLS.map((url) => ({ urls: url }))
+        // ...LOCALHOST_PASSIVE_VIDEO_ICE_SERVERS,
+        // ...DEFAULT_PASSIVE_VIDEO_ICE_SERVER_URLS.map((url) => ({ urls: url }))
+        ...LOCALHOST_PASSIVE_VIDEO_ICE_SERVERS
       ]
-    : DEFAULT_PASSIVE_VIDEO_ICE_SERVER_URLS.map((url) => ({ urls: url }))
+    // : DEFAULT_PASSIVE_VIDEO_ICE_SERVER_URLS.map((url) => ({ urls: url }))
+    : [
+        // ...LOCALHOST_PASSIVE_VIDEO_ICE_SERVERS,
+        // ...DEFAULT_PASSIVE_VIDEO_ICE_SERVER_URLS.map((url) => ({ urls: url }))
+        ...LOCALHOST_PASSIVE_VIDEO_ICE_SERVERS
+      ]
 
   return {
     iceServers
